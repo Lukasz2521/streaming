b1453409a310e942014c76bb1283d75e6452757a
Lukasz Radecki <lukasz.radecki94@gmail.com>
2017-05-11 00:03:03 +0200

    Rebuild forms

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -16,9 +16,6 @@ namespace streaming_inż
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/dropzonescripts").Include(
-                     "~/Scripts/dropzone/dropzone.js"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -27,10 +24,6 @@ namespace streaming_inż
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/dropzonescss").Include(
-                     "~/Scripts/dropzone/css/basic.css",
-                     "~/Scripts/dropzone/css/dropzone.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
--- Scripts/script.js
+++ Scripts/script.js
@@ -44,4 +44,23 @@
             isValidAvatarExtension: 'Invalid file extension. File must have .jpg extension.'
         }
     });
+
+
+    $('.attachment input[type="file"]')
+    .on('change', function(event) {
+        var el = $(event.target).closest('.attachment').find('.btn-file');
+
+        el
+        .find('.btn-file__actions__item')
+        .css({
+            'padding': '135px'
+        });
+
+        el
+        .find('.btn-file__preview')
+        .css({
+            'background-image': 'url(' + window.URL.createObjectURL(event.target.files[0]) + ')'
+        });
+    });
+
 });
